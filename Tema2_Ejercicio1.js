4842bf04503ff9fcadff2c6d2d956c2b7b8c72a0
Alejandro <alejandrosaezc@gmail.com>
2022-06-30 16:45:07 +0200

    Tema2_Ejercicio1

---
+++ Tema2_Ejercicio1.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Alejandro Saez",
+    33,
+    true,
+    new Date(1988, 8, 23),
+    { 
+        titulo: "El mundo de Sofía",
+        autor: "Jostein Gaarder",
+        fecha: new Date(1991, 0, 1),
+        url: "https://www.amazon.com/-/es/Jostein-Gaarder/dp/8478448152"
+      },
+  ];
