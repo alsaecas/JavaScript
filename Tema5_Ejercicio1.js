dc4878b8367ad8051ff45e2108b20b7251b9acb0
Alejandro <alejandrosaezc@gmail.com>
2022-07-03 18:07:49 +0200

    Tema5_Ejercicio1

---
+++ Tema5_Ejercicio1.js
@@ -0,0 +1,6 @@
+let alturacm=172;
+let alturam=1.72;
+let pesokg=75.6;
+let alturaRedondeo=Math.ceil(alturam);
+let pesoRedondeo=Math.floor(pesokg);
+let sonIguales = Number.MAX_VALUE + 1 == Number.MAX_VALUE;
