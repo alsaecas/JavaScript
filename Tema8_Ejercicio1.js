f5df6a994ef9639d44f4a3f647c8484a9e383a02
Alejandro Saez <alejandrosaezc@gmail.com>
2022-07-05 10:16:06 +0200

    Tema8_Ejercicio1

---
+++ Tema8_Ejercicio1.js
@@ -0,0 +1,14 @@
+function vacio(){
+    return true
+}
+
+async function promesa() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+function* pares() {
+    let id = 0
+    while(true) {
+        yield id += 2
+    }
+}
